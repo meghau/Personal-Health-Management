e3b181fe9f7564857ca76ca0f3ba14c179cf0bd3
aishu <aishu@LAPTOP-59DCEVSE>
2016-10-24 20:02:19 -0400

    Merge origin/master
    Conflicts:
    	build/classes/dbms_project1/HealthSupMainFrame$1.class
    	build/classes/dbms_project1/HealthSupMainFrame$2.class
    	build/classes/dbms_project1/HealthSupMainFrame$3.class
    	build/classes/dbms_project1/HealthSupMainFrame.class
    	build/classes/dbms_project1/Health_Supporter.class
    	build/classes/dbms_project1/Patient_Menu.class
    	build/classes/dbms_project1/Profile$1.class
    	build/classes/dbms_project1/Profile$2.class
    	build/classes/dbms_project1/Profile$3.class
    	build/classes/dbms_project1/Profile$4.class
    	build/classes/dbms_project1/Profile.class
    	nbproject/private/private.properties
    	nbproject/project.properties
    	src/dbms_project1/LoginFrame.java

--- src/dbms_project1/HSupPatientInfoFrame.java
+++ src/dbms_project1/HSupPatientInfoFrame.java
@@ -31,6 +31,8 @@ public class HSupPatientInfoFrame extends javax.swing.JFrame {
         diagnosis_button = new javax.swing.JButton();
         records_button = new javax.swing.JButton();
         alerts_button = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -47,6 +49,20 @@ public class HSupPatientInfoFrame extends javax.swing.JFrame {
 
         alerts_button.setText("Alerts");
 
+        jButton1.setText("Profile");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Exit");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -55,26 +71,30 @@ public class HSupPatientInfoFrame extends javax.swing.JFrame {
                 .addGap(119, 119, 119)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(8, 8, 8)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(diagnosis_button, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(records_button, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(alerts_button, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(diagnosis_button, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(records_button, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(alerts_button, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap(160, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(33, 33, 33)
+                .addGap(21, 21, 21)
                 .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
                 .addGap(18, 18, 18)
                 .addComponent(diagnosis_button)
                 .addGap(18, 18, 18)
                 .addComponent(records_button)
                 .addGap(18, 18, 18)
                 .addComponent(alerts_button)
-                .addContainerGap(110, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addContainerGap(40, Short.MAX_VALUE))
         );
 
         pack();
@@ -83,6 +103,20 @@ public class HSupPatientInfoFrame extends javax.swing.JFrame {
     private void diagnosis_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_diagnosis_buttonActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_diagnosis_buttonActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String id = HealthSupMainFrame.getPid();
+        String[] idstr = id.split(" ");
+        System.out.println(idstr[1]);
+        Profile pr = new Profile(idstr[1]);
+        pr.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.setVisible(false);
+        HealthSupMainFrame hsmenu = new HealthSupMainFrame();
+        hsmenu.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -122,6 +156,8 @@ public class HSupPatientInfoFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton alerts_button;
     private javax.swing.JButton diagnosis_button;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JButton records_button;
     // End of variables declaration//GEN-END:variables
